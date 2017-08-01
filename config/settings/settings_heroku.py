acc6fbb5ad20d30f5a0c9e5ebe76502af37340c1
Jack Cushman <jcushman@law.harvard.edu>
2017-08-01 12:45:16 -0400

    Heroku mail settings

--- config/settings/settings_heroku.py
+++ config/settings/settings_heroku.py
@@ -5,3 +5,11 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': env.db()
 }
+
+EMAIL_HOST = 'smtp.mailgun.org'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'postmaster@canifairuseit.com'
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+
+ADMINS = env.json('ADMINS')
